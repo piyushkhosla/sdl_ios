3eb93f2acf3549d481a8565e5e27470db73316c7
skorzeli <skorzeli@ford.com>
2014-08-26 04:07:24 -0400

    Added missing FMCHexUtility

---
+++ AppLink/AppLink/FMCHexUtility.h
@@ -0,0 +1,14 @@
+//
+//  FMCHexUtility.h
+//  AppLink
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FMCHexUtility : NSObject
+
++(NSString*) getHexString:(UInt8*)bytes length:(int) length;
++(NSString*) getHexString:(NSData*)data;
+
+
+@end
