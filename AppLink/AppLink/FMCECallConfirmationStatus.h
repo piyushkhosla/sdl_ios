695f53e38954f9e8f15bb8c107498aef8a6bcb24
Hansen, Kirk <khanse23@ford.com>
2014-10-01 14:16:54 -0400

    Added definitions and version numbers to FMCCareModeStatus, FMCCallConfirmationStatus, FMCEmergencyEventType,FMCFuelCutoffStatus, and FMCImageFieldName.

--- AppLink/AppLink/FMCECallConfirmationStatus.h
+++ AppLink/AppLink/FMCECallConfirmationStatus.h
@@ -5,17 +5,49 @@
 #import <Foundation/Foundation.h>
 #import <AppLink/FMCEnum.h>
 
+/** Reflects the status of the eCall Notification.
+ *<b>Since</b> AppLink 2.0
+ <p>
+ */
+
 @interface FMCECallConfirmationStatus : FMCEnum {}
 
 +(FMCECallConfirmationStatus*) valueOf:(NSString*) value;
 +(NSMutableArray*) values;
 
+/** No E-Call signal triggered.
+ */
+
 +(FMCECallConfirmationStatus*) NORMAL;
+
+/** An E-Call is being in progress.
+ */
+
 +(FMCECallConfirmationStatus*) CALL_IN_PROGRESS;
+
+/** CALL_CANCELLED	An E-Call was cancelled by the user.
+ */
+
 +(FMCECallConfirmationStatus*) CALL_CANCELLED;
+
+/** The E-Call sequence is completed.
+ */
+
 +(FMCECallConfirmationStatus*) CALL_COMPLETED;
+
+/** An E-Call could not be connected.
+ */
+
 +(FMCECallConfirmationStatus*) CALL_UNSUCCESSFUL;
+
+/** E-Call is not configured on this vehicle.
+ */
+
 +(FMCECallConfirmationStatus*) ECALL_CONFIGURED_OFF;
+
+/** E-Call is considered to be complete without Emergency Operator contact.
+ */
+
 +(FMCECallConfirmationStatus*) CALL_COMPLETE_DTMF_TIMEOUT;
 
 @end
--- AppLink/AppLink/FMCFuelCutoffStatus.h
+++ AppLink/AppLink/FMCFuelCutoffStatus.h
@@ -4,14 +4,31 @@
 
 #import <Foundation/Foundation.h>
 #import <AppLink/FMCEnum.h>
-
+/** Reflects the status of the Restraints Control Module fuel pump cutoff.<br> The fuel pump is cut off typically after the vehicle has had a collision.
+ * <p>
+ *
+ * @Since AppLink 2.0
+ *
+ */
 @interface FMCFuelCutoffStatus : FMCEnum {}
 
 +(FMCFuelCutoffStatus*) valueOf:(NSString*) value;
 +(NSMutableArray*) values;
 
+/** Fuel is cut off
+ */
+
 +(FMCFuelCutoffStatus*) TERMINATE_FUEL;
+
+/** Fuel is not cut off
+ *
+ */
 +(FMCFuelCutoffStatus*) NORMAL_OPERATION;
+
+/** Status of the fuel pump cannot be determined
+ *
+ */
+
 +(FMCFuelCutoffStatus*) FAULT;
 
 @end
