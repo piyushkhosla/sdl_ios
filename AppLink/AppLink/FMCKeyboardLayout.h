7c9aa584b0008435d3dd1f74b18dd723eaf44728
Hansen, Kirk <khanse23@ford.com>
2014-10-02 12:52:27 -0400

    Added descriptions and version numbers to FMCAAmbientLightStatus, FMCKeyboardEvent, FMCKeyboardLayout, FMCKeyPressMode, and FMCLayoutMode.

--- AppLink/AppLink/FMCKeyboardLayout.h
+++ AppLink/AppLink/FMCKeyboardLayout.h
@@ -5,13 +5,29 @@
 #import <Foundation/Foundation.h>
 #import <AppLink/FMCEnum.h>
 
+/** Enumeration listing possible keyboard layouts
+*
+*<b>Since</b> AppLink 3.0
+*
+*/
 @interface FMCKeyboardLayout : FMCEnum {}
 
 +(FMCKeyboardLayout*) valueOf:(NSString*) value;
 +(NSMutableArray*) values;
-
+/** QWERTY layout (the name comes from the first six keys<br> appearing on the top left letter row of the keyboard and read from left to right)
+ *
+ */
 +(FMCKeyboardLayout*) QWERTY;
+
+/** QWERTZ layout (the name comes from the first six keys<br> appearing on the top left letter row of the keyboard and read from left to right)
+ *
+ */
 +(FMCKeyboardLayout*) QWERTZ;
+
+/** AZERTY layout (the name comes from the first six keys<br> appearing on the top left letter row of the keyboard and read from left to right)
+ *
+ */
+
 +(FMCKeyboardLayout*) AZERTY;
 
 @end
--- AppLink/AppLink/FMCLayoutMode.h
+++ AppLink/AppLink/FMCLayoutMode.h
@@ -4,16 +4,42 @@
 
 #import <Foundation/Foundation.h>
 #import <AppLink/FMCEnum.h>
-
+/** For touchscreen interactions, the mode of how the choices are presented.
+ *
+ * <b>Since:</B> AppLink 3.0
+ *
+ */
 @interface FMCLayoutMode : FMCEnum {}
 
 +(FMCLayoutMode*) valueOf:(NSString*) value;
 +(NSMutableArray*) values;
 
+/** This mode causes the interaction to display the previous set of choices as icons.
+ *
+ */
 +(FMCLayoutMode*) ICON_ONLY;
+
+/** This mode causes the interaction to display the previous set of choices as icons along with a search field in the HMI.
+ *
+ */
 +(FMCLayoutMode*) ICON_WITH_SEARCH;
+
+/** This mode causes the interaction to display the previous set of choices as a list.
+ *
+ */
+
 +(FMCLayoutMode*) LIST_ONLY;
+
+/** This mode causes the interaction to display the previous set of choices as a list along with a search field in the HMI.
+ *
+ */
+
 +(FMCLayoutMode*) LIST_WITH_SEARCH;
+
+/** This mode causes the interaction to immediately display a keyboard entry through the HMI.
+ *
+ */
+
 +(FMCLayoutMode*) KEYBOARD;
 
 @end
