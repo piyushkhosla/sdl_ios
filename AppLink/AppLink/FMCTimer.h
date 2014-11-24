929a1752cbfb3ec48a4193e618a9f05e3b24c8fa
Aaron DeGrow <adegrow@ford.com>
2014-11-24 10:31:50 -0500

    Implemented FMCTimer

--- AppLink/AppLink/FMCTimer.h
+++ AppLink/AppLink/FMCTimer.h
@@ -11,6 +11,7 @@
 @property (assign) void (^canceledBlock)(void);
 @property (assign) float duration;
 
+- (id)init;
 - (id)initWithDuration:(float)duration;
 - (void)start;
 - (void)cancel;
--- AppLink/AppLink/FMCTimer.m
+++ AppLink/AppLink/FMCTimer.m
@@ -9,33 +9,63 @@
 @interface FMCTimer ()
 
 @property (strong) NSTimer *timer;
+@property (assign) BOOL timerRunning;
 
 @end
 
 
 @implementation FMCTimer
 
+- (id)init
+{
+    if (self = [super init]) {
+        self.duration = 0;
+        self.timerRunning = NO;
+    }
+    return self;
+}
+
 - (id)initWithDuration:(float)duration
 {
     if (self = [super init]) {
         self.duration = duration;
+        self.timerRunning = NO;
     }
     return self;
 }
 
 - (void)start
 {
-    
+    if (self.duration > 0) {
+        [self stopAndDestroyTimer];
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:self.duration target:self selector:@selector(timerElapsed) userInfo:nil repeats:NO];
+        self.timerRunning = YES;
+    }
 }
 
 - (void)cancel
 {
-    
+    [self stopAndDestroyTimer];
+    if (self.timerRunning && self.canceledBlock != nil) {
+        self.canceledBlock();
+    }
+    self.timerRunning = NO;
 }
 
 - (void)timerElapsed
 {
-    
+    [self stopAndDestroyTimer];
+    if (self.elapsedBlock != nil) {
+        self.elapsedBlock();
+    }
+    self.timerRunning = NO;
+}
+
+-(void)stopAndDestroyTimer {
+    if (self.timer != nil) {
+        [self.timer invalidate];
+        self.timer = nil;
+    }
 }
 
 @end
