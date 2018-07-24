564b01da3be210d7921fecd79a36d7296d38b2f7
piyushkhosla <piyush_khosla@infosys.com>
2018-07-24 09:01:42 +0530

    adding deprecated message to older init method.

--- SmartDeviceLink/SDLRadioControlCapabilities.h
+++ SmartDeviceLink/SDLRadioControlCapabilities.h
@@ -11,6 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @interface SDLRadioControlCapabilities : SDLRPCStruct
 
+- (instancetype)initWithModuleName:(NSString *)moduleName radioEnableAvailable:(BOOL)radioEnableAvailable radioBandAvailable:(BOOL)radioBandAvailable radioFrequencyAvailable:(BOOL)radioFrequencyAvailable hdChannelAvailable:(BOOL)hdChannelAvailable rdsDataAvailable:(BOOL)rdsDataAvailable availableHDsAvailable:(BOOL)availableHDsAvailable stateAvailable:(BOOL)stateAvailable signalStrengthAvailable:(BOOL)signalStrengthAvailable signalChangeThresholdAvailable:(BOOL)signalChangeThresholdAvailable __deprecated_msg("Use initWithModuleName: radioEnableAvailable: radioBandAvailable: radioFrequencyAvailable: hdChannelAvailable: rdsDataAvailable: availableHDsAvailable: stateAvailable: signalStrengthAvailable:signalStrengthAvailable signalChangeThresholdAvailable: hdRadioEnableAvailable: siriusxmRadioAvailable: instead");
+
 - (instancetype)initWithModuleName:(NSString *)moduleName radioEnableAvailable:(BOOL)radioEnableAvailable radioBandAvailable:(BOOL)radioBandAvailable radioFrequencyAvailable:(BOOL)radioFrequencyAvailable hdChannelAvailable:(BOOL)hdChannelAvailable rdsDataAvailable:(BOOL)rdsDataAvailable availableHDsAvailable:(BOOL)availableHDsAvailable stateAvailable:(BOOL)stateAvailable signalStrengthAvailable:(BOOL)signalStrengthAvailable signalChangeThresholdAvailable:(BOOL)signalChangeThresholdAvailable hdRadioEnableAvailable:(BOOL)hdRadioEnableAvailable siriusxmRadioAvailable:(BOOL)siriusxmRadioAvailable;
 
 /**
